15010e746f64cbf3f87dd029ea80cdd435a1af0a
kushkawle <khushwantkawle@gmail.com>
2025-07-19 12:34:52 +0530

    viewing sign-in and sign-up

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,8 +3,8 @@ import { redirect } from 'next/navigation';
 import { ReactNode } from 'react'
 
 const AuthLayout = async ({children}:{children:ReactNode}) => {
-  //const isUserAuthenticated = await isAuthenticated();
-   //if(isUserAuthenticated)  redirect('/');
+  const isUserAuthenticated = await isAuthenticated();
+   if(isUserAuthenticated)  redirect('/');
   return (
     <div className="auth-layout">{children}</div>
   )
