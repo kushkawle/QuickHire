fe2c042e1f00d4f297471020b2b794268625509d
Khushwant Kawle <143870393+kushkawle@users.noreply.github.com>
2025-08-02 12:31:37 +0530

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,27 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  // authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-
-  apiKey: "AIzaSyCMWKrmGD6NMsoy6nb8Ifke98mSuIis8Oo",
-  //apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "prepwise-91fdd.firebaseapp.com",
-  //authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: "prepwise-91fdd",
-  //projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: "prepwise-91fdd.firebasestorage.app",
-  //storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "684393486940",
-  //messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: "1:684393486940:web:3c6b4582072fcebcf96499",
-  //appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  //measurementId: "G-BNLD3MNN8K"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
