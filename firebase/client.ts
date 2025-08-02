4cab60d7978b6244f5330a0db7eea9737df296e3
Khushwant Kawle <143870393+kushkawle@users.noreply.github.com>
2025-08-02 14:45:38 +0530

    final   client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
